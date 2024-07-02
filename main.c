c38327b04eecd0ee576ca9c250984e65b330604d
Sky1sBloo <ultimate8gaming@gmail.com>
2024-07-02 16:08:31 +0800

    Fixed conflicts

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ void showHelpPrint()
 			"--display [filename]                       | Display current plane contents\n"
 			"--go-to-seat [filename] [column] [row]     | Goes to the seat of the plane\n"
 			"--clear-current-seat [filename]            | Clears the current seat assigned to the plane\n"
+			"--move-to-seat [filename] [column] [row]   | Moves to the new seat\n"
 			);
 
 }
@@ -73,13 +74,22 @@ int main(int argc, char* argv[])
 		}
 		else if (strcmp(argv[i], "--go-to-seat") == 0)
 		{
-			if (!argumentIsSupplied(argc, i, 2))  return 1;
+			if (!argumentIsSupplied(argc, i, 3))  return 1;
 			const char* fileName = argv[++i];
 			const char* column = argv[++i];
 			const char* row = argv[++i];
 			
 
 			inputGoToSeat(fileName, column, row);
+		}
+		else if (strcmp(argv[i], "--move-to-seat") == 0)
+		{
+			if (!argumentIsSupplied(argc, i, 3))  return 1;
+			const char* fileName = argv[++i];
+			const char* column = argv[++i];
+			const char* row = argv[++i];
+
+			inputMoveAccountSeat(fileName, column, row);
 		}
 		else if (strcmp(argv[i], "--clear-current-seat") == 0)
 		{
